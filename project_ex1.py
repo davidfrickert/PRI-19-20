8c767b3536fbc7d7c4c15511f8a3e1bff7ed9580
David Frickert <davidfrickert@gmail.com>
2019-10-18 12:36:43 +0100

    voltar à lógica de passar ['--documento inteiro--'] para o transform do TFIDF. mudar função de ranking de (tfidf * len(palavra)) para ( (tfidf * len(palavra))/ n_palavras)

--- project_ex1.py
+++ project_ex1.py
@@ -8,23 +8,25 @@ def get_tfidf_score(doc):
     train = fetch_20newsgroups(subset='train', remove=('headers', 'footers', 'quotes'))
     vec = TfidfVectorizer(ngram_range=(1, 3), stop_words='english')
     vec.fit(train.data)
-    testdata = vec.transform(doc).sum(0)
+
+    tf_idf_matrix = vec.transform(doc)
 
     terms = vec.get_feature_names()
 
     data = []
 
     for col, term in enumerate(terms):
-        if testdata[0, col] != 0:
-            data.append((term, testdata[0, col] * len(term)))
+        if tf_idf_matrix[0, col] != 0:
+            data.append((term, tf_idf_matrix[0, col] * (len(term) / len(term.split()) )))
 
     ranking = pd.DataFrame(data, columns=['term', 'rank'])
-    print(ranking.sort_values('rank', ascending=False))
+    print(ranking.sort_values('rank', ascending=False).to_string())
 
 
 with open('text1.txt', 'r') as doc:
-    # sent = ' '.join(doc.readlines()).lower()
-    sent = nltk.sent_tokenize('\n'.join(doc.readlines()))
+    sent = [' '.join(doc.readlines())]
+    # sent = nltk.sent_tokenize('\n'.join(doc.readlines()))
+    print(sent)
 
 
 print(get_tfidf_score(sent))
