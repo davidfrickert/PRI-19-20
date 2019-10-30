c2165a40c58e160b83e846f50bec1734f6c193db
David Frickert <davidfrickert@gmail.com>
2019-10-30 19:27:58 +0000

    Ex2 Remover deprecated Adicionar sort dos ficheiros XML para estar de acordo com os dos resultados

--- project_ex2.py
+++ project_ex2.py
@@ -5,53 +5,19 @@ from xml.dom.minidom import parse, parseString
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-from os.path import  splitext
+from os.path import splitext
+
 
 def average(dic):
     total = 0
     for item in dic:
         total += dic[item]
-    
+
     return total / len(dic)
 
-def calcTPFNDeprecated(doc, reference_results, results, at = None):
-
-    porter = PorterStemmer()
-    true_positives = 0
-    false_negatives = 0
-    counter = 0
-
-    
-    non_relevant = results[doc][len(reference_results[doc]):]
-    results[doc] = results[doc][:len(reference_results[doc])]
-
-    if(at != None) :
-        non_relevant = results[doc][at:]
-        results[doc] = results[doc][:at]
-
-    for term in reference_results[doc]:
-        
-        if at == counter:
-            return true_positives, false_negatives
-
-        for word in results[doc]:
-            if porter.stem(word[0]) == term[0]:
-                true_positives += 1
-                break
-
-        for word in non_relevant:
-            if porter.stem(word[0]) == term[0]:
-                false_negatives += 1
-                break
-        
-        counter+=1
-
-    return true_positives, false_negatives
 
 def calcTPFNFP(doc, reference_results, results):
-
-    #print("\n", doc)
-    
+    # print("\n", doc)
     porter = PorterStemmer()
     true_positives = 0
     false_negatives = 0
@@ -59,7 +25,7 @@ def calcTPFNFP(doc, reference_results, results):
 
     results[doc] = results[doc][:len(reference_results[doc])]
 
-    #true positives = achamos relevantes e é relevante
+    # true positives = achamos relevantes e é relevante
     for word in results[doc]:
         flag = 0
 
@@ -80,17 +46,14 @@ def calcTPFNFP(doc, reference_results, results):
 
         if flag == 0:
             false_negatives += 1
-            
 
-    
-    #false_negatives = len(reference_results[doc]) - true_positives
+    # false_negatives = len(reference_results[doc]) - true_positives
     return true_positives, false_negatives, false_positives
 
+
 def precisionAt(doc, reference_results, results, at):
-    
     porter = PorterStemmer()
     true_positives = 0
-    false_negatives = 0
     counter = 0
 
     tmp = results[doc][:at]
@@ -101,10 +64,10 @@ def precisionAt(doc, reference_results, results, at):
             if porter.stem(word[0]) == term[0]:
                 true_positives += 1
                 break
-    
-        counter+=1
 
-    return float(true_positives)/float(at)
+        counter += 1
+
+    return float(true_positives) / float(at)
 
 
 def calcMetrics(results, reference):
@@ -115,21 +78,19 @@ def calcMetrics(results, reference):
         recall = {}
         f1 = {}
         precision5 = {}
-        non_relevant = {}
 
         for x in reference_results:
 
-            precision5[x] = precisionAt(x, reference_results, results,5)
+            precision5[x] = precisionAt(x, reference_results, results, 5)
 
             true_positives, false_negatives, false_positives = calcTPFNFP(x, reference_results, results)
-            
+
             # TP / (TP + FP)
             # TP => corretos
             # FP => total(resultados) - corretos => incorretos
 
             precision[x] = float(true_positives) / float(true_positives + false_positives)
 
-            
             # TP / (TP + FN)
             # TP => corretos
             # FN => quais keyphrases o nosso algoritmo identificou como não-relevante mas é relevante
@@ -150,11 +111,10 @@ def calcMetrics(results, reference):
         print(f1)
         print(average(f1), end="\n\n")
 
-        #for x in reference_results:
+        # for x in reference_results:
 
         #    true_positives, false_negatives = calcTPFN(x, reference_results, results, 5)
         #    precision5[x] = float(true_positives) / float(true_positives + (len(reference_results[x]) - true_positives))
-        
 
         print("Precision@5: ")
         print(precision5)
@@ -169,8 +129,10 @@ def convertXML(xml):
     for i, sentence in enumerate(xml):
         tokens = sentence.getElementsByTagName('token')
         result += ' '.join([t.getElementsByTagName('lemma')[0].firstChild.nodeValue for t in tokens])
+        result += ' '
 
     return result
+
 
 def convertXMLToTaggedSents(xml):
     result = []
@@ -178,7 +140,7 @@ def convertXMLToTaggedSents(xml):
     for i, sentence in enumerate(xml):
         tokens = sentence.getElementsByTagName('token')
         result.append([(t.getElementsByTagName('lemma')[0].firstChild.nodeValue,
-                        t.getElementsByTagName('POS')[0].firstChild.nodeValue) for t in tokens ])
+                        t.getElementsByTagName('POS')[0].firstChild.nodeValue) for t in tokens])
     return result
 
 
@@ -186,10 +148,12 @@ def getDataFromDir(path, mode='string'):
     directory = os.fsencode(path)
 
     docs = {}
+    files = os.listdir(directory)
+    files.sort()
 
-    for f in os.listdir(directory):
+    for f in files:
+
         filePath = path + '/' + f.decode("utf-8")
-
         with open(filePath) as datasource:
             dom = parse(datasource)
             xml = dom.getElementsByTagName('sentence')
@@ -211,7 +175,7 @@ def merge(dataset, terms, scoreArr):
 
             tf_idf = scoreArr[doc_index][word_index]
             if tf_idf != 0:
-                doc_info.append((term, tf_idf * (len(term)/len(term.split(' ')))))
+                doc_info.append((term, tf_idf * (len(term) / len(term.split(' ')))))
 
         # sort por tf_idf; elem = (term, tf_idf); elem[1] = tf_idf
         doc_info.sort(key=lambda elem: elem[1], reverse=True)
@@ -219,31 +183,33 @@ def merge(dataset, terms, scoreArr):
         data.update({doc_name: doc_info})
     return data
 
+
 def getTFIDFScore(dataset):
     stopW = set(stopwords.words('english'))
 
     vec = TfidfVectorizer(stop_words=stopW, ngram_range=(1, 3), min_df=2)
 
-    #print("start")
+    # print("start")
 
     X = vec.fit_transform(dataset.values())
 
-    #print("trained")
+    # print("trained")
 
     terms = vec.get_feature_names()
     scoreArr = X.toarray()
-    
+
     return merge(dataset, terms, scoreArr)
 
+
 def main():
-    #print("begin")
+    # print("begin")
     test = getDataFromDir('ake-datasets-master/datasets/500N-KPCrowd/test')
-    #print("gottem")
+    # print("gottem")
     data = getTFIDFScore(test)
 
-    #print("start calcs")
+    # print("start calcs")
     calcMetrics(data, 'ake-datasets-master/datasets/500N-KPCrowd/references/test.reader.stem.json')
-    
+
 
 if __name__ == '__main__':
     main()
