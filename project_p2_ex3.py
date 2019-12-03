44993a2461552693c618d62a41423f6040bf50e7
davidfrickert <davidjfrickert@gmail.com>
2019-12-03 18:31:31 +0000

    Ex3. Sem usar o pagerank.

--- project_p2_ex3.py
+++ project_p2_ex3.py
@@ -5,15 +5,17 @@ from typing import List, Dict, Any
 import networkx
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+from project_ex3 import getAllCandidates, listOfTaggedToString
 from project_p2_ex1 import Helper, buildGramsUpToN
 from project_p2_ex2 import buildGraph, getInfo, computeWeightedPR
+import numpy as np
 
 
-def calculateParameters(g: networkx.Graph, doc: str, scores: Dict[str, float], pr: Dict[str, float]):
+def calculateParameters(doc: str, scores: Dict[str, float], cands, pr: Dict[str, float] = None):
     params = []
 
     max_cand_score = max(scores.values())
-    all_cands = g.nodes
+    all_cands = cands
     for cand in all_cands:
 
         freq = doc.count(cand)
@@ -33,7 +35,10 @@ def calculateParameters(g: networkx.Graph, doc: str, scores: Dict[str, float], p
             spread = 0.
         else:
             spread = last_match - first_match
-        params.append([cand_score, freq, cand_len, cand_term_count, first_match, last_match, spread, pr[cand]])
+        params.append([cand_score, cand_len, cand_term_count, first_match, last_match, spread])#, r[cand]])
+
+    params = np.array(params)
+    params /= params.max(axis=0)
     return dict(zip(all_cands, params))
 
 
@@ -45,24 +50,32 @@ def getRecipRankFusionScore(words):
             wordScore.append(1 / (50 + param))
         RRFScore.update({name: sum(wordScore)})
 
-    return Helper.dictToOrderedList(RRFScore, rev=True)[:5]
+    return dict(Helper.dictToOrderedList(RRFScore, rev=True)[:50])
 
 
 def main():
-    test = Helper.getDataFromDir('ake-datasets-master/datasets/500N-KPCrowd/test')
-    test = dict(zip(list(test.keys())[:1], list(test.values())[:1]))
-    info = getInfo()
-
+    test = Helper.getDataFromDir('ake-datasets-master/datasets/500N-KPCrowd/test', mode='list')
+    testStr = listOfTaggedToString(test)
+    #test = dict(zip(list(test.keys()), list(test.values())))
+    info = getInfo(test)
+    cands = getAllCandidates(test)
     rrfScores = {}
 
     for i, doc_name in enumerate(test.keys()):
-        g = buildGraph(test[doc_name], info['model'])
-        pr = computeWeightedPR(g, i, info, n_iter=15)
-        params = calculateParameters(g, test[doc_name], dict(zip(info['terms'], info['TF-IDF'][i])), pr)
-        rrfScores[doc_name] = getRecipRankFusionScore(params)
+        # cands =  buildGramsUpToN(test[doc_name], 3)
+        # g = buildGraph(cands, info['model'])
+        #pr = computeWeightedPR(g, i, info, n_iter=15)
+        params = calculateParameters(testStr[i], info['score'][doc_name], cands[i])#, pr)
+        print(params)
+        rrfScores[doc_name] = list(getRecipRankFusionScore(params).keys())
 
     print('rrfScores', rrfScores)
-
+    meanAPre, meanPre, meanRe, meanF1 = Helper.results(rrfScores, 'ake-datasets-master/datasets/500N-KPCrowd/references'
+                                                            '/test.reader.stem.json')
+    print(f'Mean Avg Pre for {len(rrfScores.keys())} documents: ', meanAPre)
+    print(f'Mean Precision for {len(rrfScores.keys())} documents: ', meanPre)
+    print(f'Mean Recall for {len(rrfScores.keys())} documents: ', meanRe)
+    print(f'Mean F1 for {len(rrfScores.keys())} documents: ', meanF1)
 
 if __name__ == '__main__':
     import time
